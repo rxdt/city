f87031f728ddf50d2636965fcaa910309fad8b16
Guillermo <guillewu@gmail.com>
2014-11-21 14:22:09 -0600

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Roles
+
+Role.create name: 'investor'
+Role.create name: 'admin'
 
 
-u = User.build({ email: 'investor@test.org',
-                  password: 'password', password_confirmation: 'password' })
-u.skip_confirmation!
-u.save
-u.add_role :investor
+# Default users
 
-u = User.build({ email: 'admin@test.org',
-                  password: 'password', password_confirmation: 'password' })
-u.skip_confirmation!
-u.save
-u.add_role :admin
+User.create(email: 'admin@test.org', password: 'password', password_confirmation: 'password').add_role :admin
+User.create(email: 'investor@test.org', password: 'password', password_confirmation: 'password').add_role :investor
