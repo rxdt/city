359d6da5a88ab79fe8e3894f474b0dc5f8b0f403
dhawt <daniel@eduschedu.com>
2014-11-25 09:44:03 -0800

    Adds some basic deals to the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,13 @@ Role.create name: 'admin'
 
 User.create(email: 'admin@test.org', password: 'password', password_confirmation: 'password').add_role :admin
 User.create(email: 'investor@test.org', password: 'password', password_confirmation: 'password').add_role :investor
+
+
+# Default deals
+Deal.create company_name: 'Company Name 1', blurb: 'lorem ipsum 1'
+Deal.create company_name: 'Company Name 2', blurb: 'lorem ipsum 2'
+Deal.create company_name: 'Company Name 3', blurb: 'lorem ipsum 3'
+Deal.create company_name: 'Company Name 4', blurb: 'lorem ipsum 4'
+Deal.create company_name: 'Company Name 5', blurb: 'lorem ipsum 5'
+Deal.create company_name: 'Company Name 6', blurb: 'lorem ipsum 6'
+Deal.create company_name: 'Company Name 7', blurb: 'lorem ipsum 7'
